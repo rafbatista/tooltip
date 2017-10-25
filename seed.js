0a42acd0bb4d9656d0a5095e3e1faa457787f60f
rafajr91 <rah5024@gmail.com>
2017-10-25 16:24:46 -0700

    Created seed data. #1

---
+++ seed.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require('mongodb')
+
+MongoClient.connect('mongodb://localhost/library', (err, db) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+
+  const tooltips = db.collection('tooltips')
+  tooltips
+    .deleteMany({})
+    .insertMany([
+      {
+        id: 1,
+        tip: 'Saves Data'
+      },
+      {
+        id: 2,
+        tip: 'Erases Data'
+      },
+      {
+        id: 3,
+        tip: 'Edits Data'
+      },
+      {
+        id: 4,
+        tip: 'Does Nothing'
+      }
+    ])
+    .catch(err => {
+      console.error(err)
+      process.exit(1)
+    })
+    .then(() => console.log('Tips are seeded!'))
+    .then(() => db.close())
+})
